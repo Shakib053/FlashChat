096e7b9095f32054563858f39d9b89aa70722918
Kazi Tanjim Shakib <kazitanjimshakib053@gmail.com>
2025-11-18 23:26:22 +0600

    added cltyping label library

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,26 +7,38 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.text = "⚡️FlashChat"
+        //createTypeWriterAnimation()
+    }
+ 
+}
 
+extension WelcomeViewController {
+    /*
+     This function shows label character by character manually
+     */
+    
+    func createTypeWriterAnimation() {
         let title = titleLabel.text
         titleLabel.text = ""
         var newTitle = ""
         var index = 1
-   
+        
         title?.forEach { char in
             DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.3) {
                 newTitle += String(char)
                 self.titleLabel.text = newTitle
             }
+            
             index += 1
         }
     }
-
 }
